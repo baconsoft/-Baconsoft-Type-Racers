d7c3ebfdda3f5b461bac322f31d770dc301cb628
Baconator <34716421+TSHSTORE@users.noreply.github.com>
2021-11-25 12:32:16 -0600

    Update game.py

--- game.py
+++ game.py
@@ -1,10 +1,12 @@
 import random
+print("A [baconsoft] Production")
 print("Welcome to type race")
 pone = (input("Player 1,Your name is: "))
 ptwo = (input("Player 2,Your name is: "))
 r = ('Roll')
 s = (" ")
 i = " is"
+
 # Turn 1
 # Player 1 Rolls
 print("Turn 1!")
@@ -12,6 +14,7 @@ print (pone + s + r + i)
 sd = input("Are You Ready? ")
 list1 = [1, 2, 3, 4, 5, 6]
 rol = (random.choice(list1))
+print("____________________")
 print(rol)
 if rol == 4:
     print("Your Car Broke And Wont be able to be used the next turn")
@@ -20,6 +23,7 @@ print (ptwo + s + r + i)
 sd = input("Are You Ready? ")
 list1 = [1, 2, 3, 4, 5, 6]
 prol = (random.choice(list1))
+print("____________________")
 print(prol)
 if prol == 4:
     print("Your Car Broke And Wont be able to be used the next turn")
@@ -35,6 +39,7 @@ if rol != 4:
 else:
     rolq = 0
     print("Your Car Is Broken!")
+print("____________________")
 print(rolq)
 if rolq == 4:
     print("Your Car Broke And Wont be able to be used the next turn")
@@ -47,6 +52,7 @@ if prol != 4:
 else:
     prolq = 0
     print("Your Car Is Broken!")
+print("____________________")
 print(prolq)
 if prolq == 4:
     print("Your Car Broke And Wont be able to be used the next turn")
@@ -62,6 +68,7 @@ if rolq != 4:
 else:
     rolw = 0
     print("Your Car Is Broken!")
+print("____________________")
 print(rolw)
 if rolw == 4:
     print("Your Car Broke And Wont be able to be used the next turn")
@@ -74,7 +81,8 @@ if prolq != 4:
 else:
     prolw = 0
     print("Your Car Is Broken!")
-print(prolw)
+print("____________________")
+print(prolw)    
 if prolw == 4:
     print("Your Car Broke And Wont be able to be used the next turn")
 
@@ -89,6 +97,7 @@ if rolw != 4:
 else:
     role = 0
     print("Your Car Is Broken!")
+print("____________________")
 print(role)
 if role == 4:
     print("Your Car Broke And Wont be able to be used the next turn")
@@ -101,20 +110,12 @@ if prolw != 4:
 else:
     prole = 0
     print("Your Car Is Broken!")
+print("____________________")
 print(prole)
 if prole == 4:
     print("Your Car Broke And Wont be able to be used the next turn")
 
 # Turn 5
-
-# Validation Checker
-if rol + rolq + rolw + role > 20 and prol + prolq + prolw + prole > 20:
-    print("It's a tie!")
-if rol + rolq + rolw + role > 20:
-    print(pone + "Wins!")
-elif prol + prolq + prolw + prole > 20:
-    print(ptwo + "Wins!")
-else:
 # Player One  Rolls
     print("Turn 5!")
     print (pone + s + r + i)
@@ -125,6 +126,7 @@ else:
     else:
         rolr = 0
         print("Your Car Is Broken!")
+    print("____________________")
     print(rolr)
     if rolr == 4:
         print("Your Car Broke And Wont be able to be used the next turn")
@@ -137,19 +139,453 @@ else:
     else:
         prolr = 0
         print("Your Car Is Broken!")
+    print("____________________")
+    print(prolr)
+    if prolr == 4:
+        print("Your Car Broke And Wont be able to be used the next turn")
+# Final Round 1 score
+fioro = rol + rolq + rolw + role + rolr
+pfioro = prol + prolq + prolw + prole + prolr
+# Round 1 RESET
+rol and rolq and rolw and role and rolr and prol and prolq and prolw and prole and prolr == 0
+    # Turn 6 (1) (ROUND 2 STARTS)
+# Player 1 Rolls
+print("ROUND 2!")
+print("Turn 1!")
+print (pone + s + r + i)
+sd = input("Are You Ready? ")
+list1 = [1, 2, 3, 4, 5, 6]
+rol = (random.choice(list1))
+print("____________________")
+print(rol)
+if rol == 4:
+    print("Your Car Broke And Wont be able to be used the next turn")
+# Player 2 Rolls
+print (ptwo + s + r + i)
+sd = input("Are You Ready? ")
+list1 = [1, 2, 3, 4, 5, 6]
+prol = (random.choice(list1))
+print("____________________")
+print(prol)
+if prol == 4:
+    print("Your Car Broke And Wont be able to be used the next turn")
+
+# Turn 7
+# Player 1 Rolls
+print("Turn 2!")
+print (pone + s + r + i)
+if rol != 4:
+    sd = input("Are You Ready? ")
+    list1 = [1, 2, 3, 4, 5, 6]
+    rolq = (random.choice(list1))
+else:
+    rolq = 0
+    print("Your Car Is Broken!")
+print("____________________")
+print(rolq)
+if rolq == 4:
+    print("Your Car Broke And Wont be able to be used the next turn")
+# Player 2 Rolls
+print (ptwo + s + r + i)
+if prol != 4:
+    sd = input("Are You Ready? ")
+    list1 = [1, 2, 3, 4, 5, 6]
+    prolq = (random.choice(list1))
+else:
+    prolq = 0
+    print("Your Car Is Broken!")
+print("____________________")
+print(prolq)
+if prolq == 4:
+    print("Your Car Broke And Wont be able to be used the next turn")
+
+# Turn 8
+print("Turn 3!")
+# Player 1 Rolls
+print (pone + s + r + i)
+if rolq != 4:
+    sd = input("Are You Ready? ")
+    list1 = [1, 2, 3, 4, 5, 6]
+    rolw = (random.choice(list1))
+else:
+    rolw = 0
+    print("Your Car Is Broken!")
+print("____________________")
+print(rolw)
+if rolw == 4:
+    print("Your Car Broke And Wont be able to be used the next turn")
+# Player 2 Rolls
+print (ptwo + s + r + i)
+if prolq != 4:
+    sd = input("Are You Ready? ")
+    list1 = [1, 2, 3, 4, 5, 6]
+    prolw = (random.choice(list1))
+else:
+    prolw = 0
+    print("Your Car Is Broken!")
+print("___________________")
+print(prolw)    
+if prolw == 4:
+    print("Your Car Broke And Wont be able to be used the next turn")
+
+# Turn 9
+# Player 1 Rolls
+print("Turn 4!")
+print (pone + s + r + i)
+if rolw != 4:
+    sd = input("Are You Ready? ")
+    list1 = [1, 2, 3, 4, 5, 6]
+    role = (random.choice(list1))
+else:
+    role = 0
+    print("Your Car Is Broken!")
+print("____________________")
+print(role)
+if role == 4:
+    print("Your Car Broke And Wont be able to be used the next turn")
+# Player 2 Rolls
+print (ptwo + s + r + i)
+if prolw != 4:
+    sd = input("Are You Ready? ")
+    list1 = [1, 2, 3, 4, 5, 6]
+    prole = (random.choice(list1))
+else:
+    prole = 0
+    print("Your Car Is Broken!")
+print("____________________")
+print(prole)
+if prole == 4:
+    print("Your Car Broke And Wont be able to be used the next turn")
+
+# Turn 10
+# Player One  Rolls
+    print("Turn 5!")
+    print (pone + s + r + i)
+    if role != 4:
+        sd = input("Are You Ready? ")
+        list1 = [1, 2, 3, 4, 5, 6]
+        rolr = (random.choice(list1))
+    else:
+        rolr = 0
+        print("Your Car Is Broken!")
+    print("____________________")
+    print(rolr)
+    if rolr == 4:
+        print("Your Car Broke And Wont be able to be used the next turn")
+    # Player 2 Rolls
+    print (ptwo + s + r + i)
+    if prole != 4:
+        sd = input("Are You Ready? ")
+        list1 = [1, 2, 3, 4, 5, 6]
+        prolr = (random.choice(list1))
+    else:
+        prolr = 0
+        print("Your Car Is Broken!")
+    print("____________________")
     print(prolr)
     if prolr == 4:
         print("Your Car Broke And Wont be able to be used the next turn")
 
-# Turn FINAL CHECK
+# Final Round 2 score
+fiort = rol + rolq + rolw + role + rolr
+pfiort = prol + prolq + prolw + prole + prolr
+# Round 3 RESET
+rol and rolq and rolw and role and rolr and prol and prolq and prolw and prole and prolr == 0
+# Turn 1
+# START OF ROUND 4
+# Player 1 Rolls
+print("Turn 1!")
+print (pone + s + r + i)
+sd = input("Are You Ready? ")
+list1 = [1, 2, 3, 4, 5, 6]
+rol = (random.choice(list1))
+print("____________________")
+print(rol)
+if rol == 4:
+    print("Your Car Broke And Wont be able to be used the next turn")
+# Player 2 Rolls
+print (ptwo + s + r + i)
+sd = input("Are You Ready? ")
+list1 = [1, 2, 3, 4, 5, 6]
+prol = (random.choice(list1))
+print("____________________")
+print(prol)
+if prol == 4:
+    print("Your Car Broke And Wont be able to be used the next turn")
 
+# Turn 2
+# Player 1 Rolls
+print("Turn 2!")
+print (pone + s + r + i)
+if rol != 4:
+    sd = input("Are You Ready? ")
+    list1 = [1, 2, 3, 4, 5, 6]
+    rolq = (random.choice(list1))
+else:
+    rolq = 0
+    print("Your Car Is Broken!")
+print("____________________")
+print(rolq)
+if rolq == 4:
+    print("Your Car Broke And Wont be able to be used the next turn")
+# Player 2 Rolls
+print (ptwo + s + r + i)
+if prol != 4:
+    sd = input("Are You Ready? ")
+    list1 = [1, 2, 3, 4, 5, 6]
+    prolq = (random.choice(list1))
+else:
+    prolq = 0
+    print("Your Car Is Broken!")
+print("____________________")
+print(prolq)
+if prolq == 4:
+    print("Your Car Broke And Wont be able to be used the next turn")
+
+# Turn 3
+print("Turn 3!")
+# Player 1 Rolls
+print (pone + s + r + i)
+if rolq != 4:
+    sd = input("Are You Ready? ")
+    list1 = [1, 2, 3, 4, 5, 6]
+    rolw = (random.choice(list1))
+else:
+    rolw = 0
+    print("Your Car Is Broken!")
+print("____________________")
+print(rolw)
+if rolw == 4:
+    print("Your Car Broke And Wont be able to be used the next turn")
+# Player 2 Rolls
+print (ptwo + s + r + i)
+if prolq != 4:
+    sd = input("Are You Ready? ")
+    list1 = [1, 2, 3, 4, 5, 6]
+    prolw = (random.choice(list1))
+else:
+    prolw = 0
+    print("Your Car Is Broken!")
+print("____________________")
+print(prolw)    
+if prolw == 4:
+    print("Your Car Broke And Wont be able to be used the next turn")
+
+# Turn 4
+# Player 1 Rolls
+print("Turn 4!")
+print (pone + s + r + i)
+if rolw != 4:
+    sd = input("Are You Ready? ")
+    list1 = [1, 2, 3, 4, 5, 6]
+    role = (random.choice(list1))
+else:
+    role = 0
+    print("Your Car Is Broken!")
+print("____________________")
+print(role)
+if role == 4:
+    print("Your Car Broke And Wont be able to be used the next turn")
+# Player 2 Rolls
+print (ptwo + s + r + i)
+if prolw != 4:
+    sd = input("Are You Ready? ")
+    list1 = [1, 2, 3, 4, 5, 6]
+    prole = (random.choice(list1))
+else:
+    prole = 0
+    print("Your Car Is Broken!")
+print("____________________")
+print(prole)
+if prole == 4:
+    print("Your Car Broke And Wont be able to be used the next turn")
+
+# Turn 5
+# Player One  Rolls
+    print("Turn 5!")
+    print (pone + s + r + i)
+    if role != 4:
+        sd = input("Are You Ready? ")
+        list1 = [1, 2, 3, 4, 5, 6]
+        rolr = (random.choice(list1))
+    else:
+        rolr = 0
+        print("Your Car Is Broken!")
+    print("____________________")
+    print(rolr)
+    if rolr == 4:
+        print("Your Car Broke And Wont be able to be used the next turn")
+    # Player 2 Rolls
+    print (ptwo + s + r + i)
+    if prole != 4:
+        sd = input("Are You Ready? ")
+        list1 = [1, 2, 3, 4, 5, 6]
+        prolr = (random.choice(list1))
+    else:
+        prolr = 0
+        print("Your Car Is Broken!")
+    print("____________________")
+    print(prolr)
+    if prolr == 4:
+        print("Your Car Broke And Wont be able to be used the next turn")
+# Final Round 4 score
+fiorf = rol + rolq + rolw + role + rolr
+pfiorf = prol + prolq + prolw + prole + prolr
+# Round 4 RESET
+rol and rolq and rolw and role and rolr and prol and prolq and prolw and prole and prolr == 0
+    
+    # Turn 7 (1) (ROUND 5 STARTS)
+# Player 1 Rolls
+print("ROUND 2!")
+print("Turn 1!")
+print (pone + s + r + i)
+sd = input("Are You Ready? ")
+list1 = [1, 2, 3, 4, 5, 6]
+rol = (random.choice(list1))
+print("____________________")
+print(rol)
+if rol == 4:
+    print("Your Car Broke And Wont be able to be used the next turn")
+# Player 2 Rolls
+print (ptwo + s + r + i)
+sd = input("Are You Ready? ")
+list1 = [1, 2, 3, 4, 5, 6]
+prol = (random.choice(list1))
+print("____________________")
+print(prol)
+if prol == 4:
+    print("Your Car Broke And Wont be able to be used the next turn")
+
+# Turn 7
+# Player 1 Rolls
+print("Turn 2!")
+print (pone + s + r + i)
+if rol != 4:
+    sd = input("Are You Ready? ")
+    list1 = [1, 2, 3, 4, 5, 6]
+    rolq = (random.choice(list1))
+else:
+    rolq = 0
+    print("Your Car Is Broken!")
+print("____________________")
+print(rolq)
+if rolq == 4:
+    print("Your Car Broke And Wont be able to be used the next turn")
+# Player 2 Rolls
+print (ptwo + s + r + i)
+if prol != 4:
+    sd = input("Are You Ready? ")
+    list1 = [1, 2, 3, 4, 5, 6]
+    prolq = (random.choice(list1))
+else:
+    prolq = 0
+    print("Your Car Is Broken!")
+print("____________________")
+print(prolq)
+if prolq == 4:
+    print("Your Car Broke And Wont be able to be used the next turn")
+
+# Turn 8
+print("Turn 3!")
+# Player 1 Rolls
+print (pone + s + r + i)
+if rolq != 4:
+    sd = input("Are You Ready? ")
+    list1 = [1, 2, 3, 4, 5, 6]
+    rolw = (random.choice(list1))
+else:
+    rolw = 0
+    print("Your Car Is Broken!")
+print("____________________")
+print(rolw)
+if rolw == 4:
+    print("Your Car Broke And Wont be able to be used the next turn")
+# Player 2 Rolls
+print (ptwo + s + r + i)
+if prolq != 4:
+    sd = input("Are You Ready? ")
+    list1 = [1, 2, 3, 4, 5, 6]
+    prolw = (random.choice(list1))
+else:
+    prolw = 0
+    print("Your Car Is Broken!")
+print("___________________")
+print(prolw)    
+if prolw == 4:
+    print("Your Car Broke And Wont be able to be used the next turn")
+
+# Turn 9
+# Player 1 Rolls
+print("Turn 4!")
+print (pone + s + r + i)
+if rolw != 4:
+    sd = input("Are You Ready? ")
+    list1 = [1, 2, 3, 4, 5, 6]
+    role = (random.choice(list1))
+else:
+    role = 0
+    print("Your Car Is Broken!")
+print("____________________")
+print(role)
+if role == 4:
+    print("Your Car Broke And Wont be able to be used the next turn")
+# Player 2 Rolls
+print (ptwo + s + r + i)
+if prolw != 4:
+    sd = input("Are You Ready? ")
+    list1 = [1, 2, 3, 4, 5, 6]
+    prole = (random.choice(list1))
+else:
+    prole = 0
+    print("Your Car Is Broken!")
+print("____________________")
+print(prole)
+if prole == 4:
+    print("Your Car Broke And Wont be able to be used the next turn")
+
+# Turn 10
+# Player One  Rolls
+    print("Turn 5!")
+    print (pone + s + r + i)
+    if role != 4:
+        sd = input("Are You Ready? ")
+        list1 = [1, 2, 3, 4, 5, 6]
+        rolr = (random.choice(list1))
+    else:
+        rolr = 0
+        print("Your Car Is Broken!")
+    print("____________________")
+    print(rolr)
+    if rolr == 4:
+        print("Your Car Broke And Wont be able to be used the next turn")
+    # Player 2 Rolls
+    print (ptwo + s + r + i)
+    if prole != 4:
+        sd = input("Are You Ready? ")
+        list1 = [1, 2, 3, 4, 5, 6]
+        prolr = (random.choice(list1))
+    else:
+        prolr = 0
+        print("Your Car Is Broken!")
+    print("____________________")
+    print(prolr)
+    if prolr == 4:
+        print("Your Car Broke And Wont be able to be used the next turn")
+
+# Final Round 5 score
+fiorff = rol + rolq + rolw + role + rolr
+pfiorff = prol + prolq + prolw + prole + prolr
+# Round 5 RESET
+rol and rolq and rolw and role and rolr and prol and prolq and prolw and prole and prolr == 0
+
+# Turn FINAL_CHECK
 # Validation Checker
-if rol + rolq + rolw + role + rolr > 20 and prol + prolq + prolw + prole + prolr > 20:
-    print("It's a tie!")
-if rol + rolq + rolw + role + rolr > 20:
+if fioro + fiort + fiorf + fiorff > 80 and pfioro + pfiort + pfiorf + pfiorff > 80:
+    print("It's a tie! Both players reached [20]")
+if fioro + fiort + fiorf + fiorff > 80:
     print(pone + "Wins!")
-elif prol + prolq + prolw + prole + prolr > 20:
+elif pfioro + pfiort + pfiorf + pfiorff > 80:
     print(ptwo + "Wins!")
 else:
-    print("It's A Tie!")
+    print("It's A Tie!, none of you reached 20")
 quit
